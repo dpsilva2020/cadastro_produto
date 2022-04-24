15f799b32f9100f3ef4bdb5594a9279d05bc387e
Daniel Pacheco da Silva <63026989+dpsilva2020@users.noreply.github.com>
2022-04-24 15:20:55 -0300

    Add files via upload

---
+++ prd_produto.py
@@ -0,0 +1,59 @@
+
+
+def opcoes():
+    if opcoes_entrada==1:
+        data_produto=input('Data de Cadastro: ')
+        tipo_produto=input('Tipo de Produto: ')
+        marca=input('Marca: ')
+        modelo=input('Modelo: ')
+        cor=input('Cor: ')
+        produto_nome=tipo_produto+' '+marca+' '+modelo+' '+cor
+        nome_produto=input(f'{produto_nome} ')        
+        valor_custo=float(input('Valor de Custo: '))
+        valor_venda=float(input('Valor Venda: '))
+        lucro_venda=valor_venda-valor_custo
+        margem_de_lucro=((lucro_venda/valor_venda)*100)
+        print('-'*30)
+        print('FICHA DE PRODUTO')
+        print('-'*30)
+        print(f'Data:{data_produto} \n Marca: {marca} \n Modelo: {modelo} \n Nome do Produto: {nome_produto} \n Tipo Produto: {tipo_produto} \n Valor Custo: {valor_custo} \n Valor Venda: {valor_venda} \n Margem de Lucro: {lucro_venda}\n Margem de Lucro: {margem_de_lucro}')
+        gravar=int(input('Deseja Gravar esse produto? \n 1-Sim ou 2-Não \n Resposta: '))
+        if gravar==1:
+            import sqlite3
+            banco= sqlite3.connect('sistema.db')
+            cursor=banco.cursor()
+            cursor.execute(F"INSERT INTO PRD_PRODUTO (DATA_PRODUTO,MARCA,MODELO,NOME_PRODUTO,TIPO_PRODUTO,VALOR_CUSTO,VALOR_VENDA) VALUES  ('{data_produto}','{marca}','{modelo}','{produto_nome}','{tipo_produto}','{valor_custo}','{valor_venda}')") 
+            banco.commit()
+            print('Produto cadastrado!')
+        else:
+            print('O produto não foi cadastrado.')
+            
+    elif opcoes_entrada==2:
+        import sqlite3
+        banco= sqlite3.connect('sistema.db')
+        cursor=banco.cursor()
+        cursor.execute("SELECT * FROM PRD_PRODUTO")
+        print(cursor.fetchall())
+        item_alterar=int(input('Informar o cd_produto desejado para alteração: '))
+        tipo_alteracao=input("informe a o nome da coluna='o que deseja alterar'")
+        cursor.execute(F"UPDATE PRD_PRODUTO SET {tipo_alteracao} WHERE CD_PRODUTO='{item_alterar}' ")
+        banco.commit()
+        cursor.execute("SELECT * FROM PRD_PRODUTO")
+        print(cursor.fetchall())
+        print('Produto Alterado!')
+    elif opcoes_entrada==3:
+        import sqlite3
+        banco= sqlite3.connect('sistema.db')
+        cursor=banco.cursor()
+        cursor.execute("SELECT * FROM PRD_PRODUTO")
+        print(cursor.fetchall())
+        cd_prd_deletar=int(input('Informe o cd_produto que será deletado: '))
+        cursor.execute(f"DELETE FROM PRD_PRODUTO WHERE CD_PRODUTO='{cd_prd_deletar}'")
+        banco.commit()
+        print(f'O produto {cd_prd_deletar} foi deletado!')
+    else:
+        print('Nenhuma Opção foi selecionada!')
+
+
+opcoes_entrada=int(input('O que deseja fazer? \n 1-Cadastrar um produto \n 2-Alterar um Cadastro \n 3-Excluir um cadastro\n Resposta:   '))
+opcoes()
